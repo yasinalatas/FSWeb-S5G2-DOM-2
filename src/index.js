9b2ff13e28a76dd4d2ee7bb4dc0df1f8767dc2c3
yasinalatas <yasinn.alatas@gmail.com>
2023-03-22 13:54:40 +0300

    ufak denemeler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
 // document.querySelector("h1").addEventListener("click",function(e){
 // alert("Bana tıkladın!")
 // });
 
-
 // Kodlar buraya gelecek!
+
+const alerted = document.querySelector(".home .content-pick .destination .btn");
+
+alerted.addEventListener("click", (event) => {
+  alert("Seni de Götürüyoruz!");
+});
+
+alerted.addEventListener("mouseover", (mouse) => {
+    alerted.getElementsByClassName.backgroundColor = "red"
+})
+
+alerted.addEventListener("mouseenter", (area) => {
+    area.style.backgroundColor = "yellow"}
+})
